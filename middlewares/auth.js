4dc85b470bbb994ff3be643445b27f33a162cc93
Sam Wilson <60236737+iDuckyFuzz@users.noreply.github.com>
2021-01-20 11:51:53 +0000

    working on auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,27 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-// const { promisify } = require("util");
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/user');
 
+exports.isLoggedIn = async (req, res, next) => {
 
-// exports.isLoggedIn =(req, ress, next) => {
-//     console.log("checking if user is logged in");
+    if (req.cookies.jwt) {
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
 
-//     if (req.cookies.jwt) {
-//         // console.log("The cookie JWT exists");
-//         const decoded = promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
-//         console.log("My token decoded");
-//         console.log(decoded);
+        req.user = await User.findById(decoded.id);
+    }
 
-//         req.userFound = await User.findById(decoded.id); 
-//     }
+    //done the required actions
+    next();
+}
 
-//     next();
-// }
-// exports.logout = (req, res, next) => {
+exports.logout = (req, res, next) => {
 
-//     res.cookies('jwt', 'logout', {
-//         expires: new Date(Date.now() + 2 * 1000),
-//         httpOnly: true
-//     });
-    
-//         next();
-// }
+    res.cookie('jwt', 'logout', {
+        expires: new Date(Date.now() + 2 * 1000),
+        httpOnly: true
+    });
+
+    //done the required actions
+    next();
+}
