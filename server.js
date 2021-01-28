7860bb9fe64aeced7f34e9bf21c56572c6939939
Sam Wilson <60236737+iDuckyFuzz@users.noreply.github.com>
2021-01-28 16:06:24 +0000

    removed .gets

--- server.js
+++ server.js
@@ -50,22 +50,6 @@ app.get('/', async (req, res) => {
     // })
 });
 
-app.get('/login', async (req, res) => {
-    res.send("This is the login page");
-});
-
-app.get('/quizsetup', async (req, res) => {
-    res.send("This is the quiz setup page");
-});
-
-app.get('/register', async (req, res) => {
-    res.send("This is the register page");
-});
-
-app.get('/leaderboard', async (req, res) => {
-    res.send("This is the leaderboard page");
-});
-
 app.get("/isauthd", async (req, res) => {
     //this is where we can pass all the data to mongodb
     try {
